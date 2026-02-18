0bc9e8d90883c8bd1c76f7a4225982c4a5b71d80
Ricardo Borda <r2b3fsa@gmail.com>
2026-02-18 19:02:33 -0300

    fix newwwww

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,9 @@ import { defineConfig } from "astro/config";
 
 const isGitHubActions = process.env.GITHUB_ACTIONS === "true";
 const repo = process.env.GITHUB_REPOSITORY?.split("/")[1] || "ricadmin";
+const owner = process.env.GITHUB_REPOSITORY_OWNER || "ricardo23b";
 
 export default defineConfig({
-  // En GitHub Pages: /<repo>
-  base: isGitHubActions ? `/${repo}` : "/",
-  site: isGitHubActions
-    ? `https://${process.env.GITHUB_REPOSITORY_OWNER}.github.io`
-    : "https://ricadmin.io",
+  base: isGitHubActions ? `/${repo}/` : "/",
+  site: isGitHubActions ? `https://${owner}.github.io` : "https://ricadmin.io",
 });
